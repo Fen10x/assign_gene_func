f72c56696f91d2f6b8dba48c38dd1d7dcb2c0970
Thomas Fenton-Lee <thomasfentonlee@gmail.com>
2026-09-22 19:12:38 +1000

    optimised global_alignment and scoring_function to make it run substantially faster. Ridiculously faster, even

--- global alignment.py
+++ global alignment.py
@@ -4,26 +4,30 @@ from Bio import SeqIO
 from Bio.Align import substitution_matrices
 from Bio import Entrez
 
+blosum62 = substitution_matrices.load("BLOSUM62") #load in the BLOSUM62 substitution matrix
+
 def global_alignment(seq1, seq2, scoring_function):
-    #define gap penalty
+    #define variables
     d = 8
+    n = len(seq1)
+    m = len(seq2)
     #initialise pointer array
-    pointer = [[None] * (len(seq2) + 1) for _ in range(len(seq1) + 1)]
+    pointer = [[None] * (m + 1) for _ in range(n + 1)]
 
     #initialise scoring matrix to 0
-    a = [[0] * (len(seq2) + 1) for _ in range(len(seq1) + 1)] 
+    a = [[0] * (m + 1) for _ in range(n + 1)] 
     #initialise left column with gap penalty
-    for i in range(1, len(seq1)+1): 
+    for i in range(1, n+1): 
         a[i][0] = -i*8
         pointer[i][0] = (i-1,0)
     #initialise top row with gap penalty
-    for j in range(1, len(seq2)+1): 
+    for j in range(1, m+1): 
         a[0][j] = -j*8
         pointer[0][j] = (0,j-1)
 
     #perform alignment
-    for i in range(1, len(seq1)+1):
-        for j in range(1, len(seq2)+1):
+    for i in range(1, n+1):
+        for j in range(1, m+1):
             match = a[i-1][j-1] + scoring_function(seq1[i-1], seq2[j-1])
             gap_x = a[i-1][j] - d
             gap_y = a[i][j-1] - d 
@@ -36,42 +40,40 @@ def global_alignment(seq1, seq2, scoring_function):
                 pointer[i][j] = (i,j-1)
   
     #traceback to construct alignment
-    i = len(seq1) 
-    j = len(seq2)
+    i = n
+    j = m
     k = 0
     identity = 0
-    seq1a = ""
-    seq2a = ""
+    seq1a = []
+    seq2a = []
     while i > 0 or j > 0:
         ip, jp = pointer[i][j]
         if ip == i:
             #gap in y
-            seq1a += "-"
-            seq2a += seq2[j-1]
+            seq1a.append("-")
+            seq2a.append(seq2[j-1])
         elif jp == j:
             #gap in x
-            seq1a += seq1[i-1]
-            seq2a += "-"
+            seq1a.append(seq1[i-1])
+            seq2a.append("-")
         else: 
             #match
             if seq1[i-1] == seq2[j-1]: identity += 1
-            seq1a += seq1[i-1]
-            seq2a += seq2[j-1]
+            seq1a.append(seq1[i-1])
+            seq2a.append(seq2[j-1])
 
         i, j = ip, jp
         k += 1
 
     id_score = 100*identity/k 
 
-    seq1a = seq1a[::-1]
-    seq2a = seq2a[::-1]
+    seq1a = "".join(reversed(seq1a))
+    seq2a = "".join(reversed(seq2a))
 
     return seq1a, seq2a, id_score
 
 def scoring_function(aa_i,aa_j):
-    blosum62 = substitution_matrices.load("BLOSUM62")
-    score = blosum62[aa_i][aa_j]
-    return (score)
+    return (blosum62[aa_i][aa_j])
 
 #scrape protein sequence from sars_cov_2.fa
 record = SeqIO.read("data/sars_cov_2.fa", "fasta")
